23139eaa90cd0c9dd693446e9eee522b5fbc8fe9
Dipak <dipakpal7408@gmail.com>
2026-07-13 20:11:54 +0530

    Add files via upload

---
+++ 01_Basics/04_even_odd.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter a number:");
+    scanf("%d", &num);
+
+    if(num % 2 == 0)
+    {
+        printf("%d is an even number.\n");
+
+    }
+
+    else
+    {
+        printf("%d is an odd number.\n");
+    }
+
+    return 0;
+}
